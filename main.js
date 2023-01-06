dbc751c05ed53f58c89fb5a4ee2290784e5e2992
marciorobertomr <marcio_mr1@hotmail.com>
2023-01-06 20:38:29 -0300

    Código de Encriptação pronto

--- main.js
+++ main.js
@@ -7,33 +7,69 @@
 // A letra "o" é convertida para "ober"
 // A letra "u" é convertida para "ufat"
 
-let texto = "ROBESVALDO TESTOU O DECODIFICADOR";
-let textoLista = [];
-
-// console.log(texto.replace("O", o));
-
-for (let i = 0; i < texto.length; i++) {
-    textoLista.push(texto.charAt(i));
-}
-
-for (let i = 0; i < textoLista.length; i++) {
-    if (textoLista[i] === "A") {
-        textoLista[i] = "AI";
-    } else if (textoLista[i] === "E") {
-        textoLista[i] = "ENTER";
-    } else if (textoLista[i] === "I") {
-        textoLista[i] = "IMES";
-    } else if (textoLista[i] === "O") {
-        textoLista[i] = "OBER";
-    } else if (textoLista[i] === "U") {
-        textoLista[i] = "UFAT";
-    }
-}
+// APRENDIZADO
 
 // com o toString, precisamos utilizar do replace para substituir os separadores.
 // /,/g captura todas as vírgulas e substitui
 // console.log(textoLista.toString().replace(/,/g, ''));
 
 // Já com o join, não precisamos fazer o uso do replace, pois já conseguimos passar o parâmetro para os separadores desejados
-console.log(textoLista);
-console.log(textoLista.join(''));
+
+function criptografar() {
+    let texto = "";
+    let textoLista = [];
+
+    texto = document.getElementById('texto').value;
+
+    for (let i = 0; i < texto.length; i++) {
+        textoLista.push(texto.charAt(i));
+    }
+
+    for (let i = 0; i < textoLista.length; i++) {
+        if (textoLista[i] === "A") {
+            textoLista[i] = "AI";
+        } else if (textoLista[i] === "E") {
+            textoLista[i] = "ENTER";
+        } else if (textoLista[i] === "I") {
+            textoLista[i] = "IMES";
+        } else if (textoLista[i] === "O") {
+            textoLista[i] = "OBER";
+        } else if (textoLista[i] === "U") {
+            textoLista[i] = "UFAT";
+        }
+    }
+    
+    document.getElementById('resultado').innerHTML += textoLista.join('');
+}
+
+function descriptografar() {
+    let texto = "";
+    let textoLista = [];
+
+    texto = document.getElementById('texto').value;
+
+    for (let i = 0; i < texto.length; i++) {
+        textoLista.push(texto.charAt(i));
+    }
+
+    for (let i = 0; i < textoLista.length; i++) {
+        if (textoLista[i] === "A" && textoLista[i+1] === "I") {
+            textoLista[i] = "A";
+            textoLista.splice(i+1, 1);
+        } else if (textoLista[i] === "E" && textoLista[i+1] === "N" && textoLista[i+2] === "T" && textoLista[i+3] === "E" && textoLista[i+4] === "R") {
+            textoLista[i] = "E";
+            textoLista.splice(i+1, 4);
+        } else if (textoLista[i] === "I" && textoLista[i+1] === "M" && textoLista[i+2] === "E" && textoLista[i+3] === "S") {
+            textoLista[i] = "I";
+            textoLista.splice(i+1, 3);
+        } else if (textoLista[i] === "O" && textoLista[i+1] === "B" && textoLista[i+2] === "E" && textoLista[i+3] === "R") {
+            textoLista[i] = "O";
+            textoLista.splice(i+1, 3);
+        } else if (textoLista[i] === "U" && textoLista[i+1] === "F" && textoLista[i+2] === "A" && textoLista[i+3] === "T") {
+            textoLista[i] = "U";
+            textoLista.splice(i+1, 3);
+        }
+    }
+
+    document.getElementById('resultado').innerHTML += textoLista.join('');
+}
