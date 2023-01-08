03fb466b26ecff83d75784db4662dd24d9afbf66
marciorobertomr <marcio_mr1@hotmail.com>
2023-01-08 13:09:40 -0300

    Ajustes realizados nos códigos. Criação de um rodapé

--- main.js
+++ main.js
@@ -25,22 +25,23 @@ function criptografar() {
     textoLista = [];
 
     texto = document.getElementById('texto').value;
+    texto = texto.toLowerCase();
 
     for (let i = 0; i < texto.length; i++) {
         textoLista.push(texto.charAt(i));
     }
 
     for (let i = 0; i < textoLista.length; i++) {
-        if (textoLista[i] === "A") {
-            textoLista[i] = "AI";
-        } else if (textoLista[i] === "E") {
-            textoLista[i] = "ENTER";
-        } else if (textoLista[i] === "I") {
-            textoLista[i] = "IMES";
-        } else if (textoLista[i] === "O") {
-            textoLista[i] = "OBER";
-        } else if (textoLista[i] === "U") {
-            textoLista[i] = "UFAT";
+        if (textoLista[i] === "a") {
+            textoLista[i] = "ai";
+        } else if (textoLista[i] === "e") {
+            textoLista[i] = "enter";
+        } else if (textoLista[i] === "i") {
+            textoLista[i] = "imes";
+        } else if (textoLista[i] === "o") {
+            textoLista[i] = "ober";
+        } else if (textoLista[i] === "u") {
+            textoLista[i] = "ufat";
         }
     }
     
@@ -53,26 +54,27 @@ function descriptografar() {
     textoLista = [];
 
     texto = document.getElementById('texto').value;
+    texto = texto.toLowerCase();
 
     for (let i = 0; i < texto.length; i++) {
         textoLista.push(texto.charAt(i));
     }
 
     for (let i = 0; i < textoLista.length; i++) {
-        if (textoLista[i] === "A" && textoLista[i+1] === "I") {
-            textoLista[i] = "A";
+        if (textoLista[i] === "a" && textoLista[i+1] === "i") {
+            textoLista[i] = "a";
             textoLista.splice(i+1, 1);
-        } else if (textoLista[i] === "E" && textoLista[i+1] === "N" && textoLista[i+2] === "T" && textoLista[i+3] === "E" && textoLista[i+4] === "R") {
-            textoLista[i] = "E";
+        } else if (textoLista[i] === "e" && textoLista[i+1] === "n" && textoLista[i+2] === "t" && textoLista[i+3] === "e" && textoLista[i+4] === "r") {
+            textoLista[i] = "e";
             textoLista.splice(i+1, 4);
-        } else if (textoLista[i] === "I" && textoLista[i+1] === "M" && textoLista[i+2] === "E" && textoLista[i+3] === "S") {
-            textoLista[i] = "I";
+        } else if (textoLista[i] === "i" && textoLista[i+1] === "m" && textoLista[i+2] === "e" && textoLista[i+3] === "s") {
+            textoLista[i] = "i";
             textoLista.splice(i+1, 3);
-        } else if (textoLista[i] === "O" && textoLista[i+1] === "B" && textoLista[i+2] === "E" && textoLista[i+3] === "R") {
-            textoLista[i] = "O";
+        } else if (textoLista[i] === "o" && textoLista[i+1] === "b" && textoLista[i+2] === "e" && textoLista[i+3] === "r") {
+            textoLista[i] = "o";
             textoLista.splice(i+1, 3);
-        } else if (textoLista[i] === "U" && textoLista[i+1] === "F" && textoLista[i+2] === "A" && textoLista[i+3] === "T") {
-            textoLista[i] = "U";
+        } else if (textoLista[i] === "u" && textoLista[i+1] === "f" && textoLista[i+2] === "a" && textoLista[i+3] === "t") {
+            textoLista[i] = "u";
             textoLista.splice(i+1, 3);
         }
     }
